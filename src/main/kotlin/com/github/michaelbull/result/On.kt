47fa20ca34edfef10eeafb1861e9af04e6e56224
Kevin Herron <kevinherron@gmail.com>
2017-12-16 14:12:07 -0800

    Inline the onSuccess() and onFailure() functions

--- src/main/kotlin/com/github/michaelbull/result/On.kt
+++ src/main/kotlin/com/github/michaelbull/result/On.kt
@@ -4,10 +4,10 @@ package com.github.michaelbull.result
  * Calls a [callback] if the [Result] is [Ok].
  * @param callback The function to call.
  */
-fun <V, E> Result<V, E>.onSuccess(callback: (V) -> Unit) = mapBoth(callback, {})
+inline fun <V, E> Result<V, E>.onSuccess(callback: (V) -> Unit) = mapBoth(callback, {})
 
 /**
  * Calls a [callback] if the [Result] is [Err].
  * @param callback The function to call.
  */
-fun <V, E> Result<V, E>.onFailure(callback: (E) -> Unit) = mapBoth({}, callback)
+inline fun <V, E> Result<V, E>.onFailure(callback: (E) -> Unit) = mapBoth({}, callback)

3a3b5415a748ad75bc133097e9d7b7cfd0d73cb8
Michael Bull <mikebull94@gmail.com>
2018-11-01 11:25:20 +0000

    Return the Result in on{Success,Failure}
    This facilitates chaining onSuccess/onFailure calls that may perform
    arbitrary side-effects, such as logging.

--- src/main/kotlin/com/github/michaelbull/result/On.kt
+++ src/main/kotlin/com/github/michaelbull/result/On.kt
@@ -1,11 +1,23 @@
 package com.github.michaelbull.result
 
 /**
- * Invokes a [callback] if this [Result] is [Ok].
+ * Invokes an [action] if this [Result] is [Ok].
  */
-inline infix fun <V, E> Result<V, E>.onSuccess(callback: (V) -> Unit) = mapBoth(callback, {})
+inline infix fun <V, E> Result<V, E>.onSuccess(action: (V) -> Unit): Result<V, E> {
+    if (this is Ok) {
+        action(value)
+    }
+
+    return this
+}
 
 /**
- * Invokes a [callback] if this [Result] is [Err].
+ * Invokes an [action] if this [Result] is [Err].
  */
-inline infix fun <V, E> Result<V, E>.onFailure(callback: (E) -> Unit) = mapBoth({}, callback)
+inline infix fun <V, E> Result<V, E>.onFailure(action: (E) -> Unit): Result<V, E> {
+    if (this is Err) {
+        action(error)
+    }
+
+    return this
+}

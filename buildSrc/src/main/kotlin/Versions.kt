620f4342203797ce934b62b1b31560ccf60f4aa7
Tristan Hamilton <tristan.hamilton@workday.com>
2020-08-28 16:19:39 +0100

    Remove unused dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
     const val dokka = "0.10.1"
     const val kotlin = "1.4.0"
     const val kotlinBenchmark = "0.2.0-dev-20"
-    const val kotlinCoroutines = "1.3.8"
+    const val kotlinCoroutines = "1.3.9"
     const val ktor = "1.4.0"
     const val logback = "1.2.3"
     const val releasePlugin = "2.8.1"

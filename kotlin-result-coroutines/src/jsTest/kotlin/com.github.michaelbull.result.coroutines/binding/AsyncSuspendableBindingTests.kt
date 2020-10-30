1bd55a07b464cfd0c43f423d236de0c0a02aba5d
Jan Müller <janmueller3698@gmail.com>
2020-10-30 20:26:21 +0100

    Updated AsyncSuspendableBindingTest for jsTest target

--- kotlin-result-coroutines/src/jsTest/kotlin/com.github.michaelbull.result.coroutines/binding/AsyncSuspendableBindingTests.kt
+++ kotlin-result-coroutines/src/jsTest/kotlin/com.github.michaelbull.result.coroutines/binding/AsyncSuspendableBindingTests.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class AsyncSuspendableBindingTest {
@@ -44,14 +45,14 @@ class AsyncSuspendableBindingTest {
     }
 
     @Test
-    fun returnsAnyErrIfBindingFailed(): dynamic {
+    fun returnsFirstErrIfBindingFailed(): dynamic {
         suspend fun provideX(): Result<Int, BindingError> {
-            delay(1)
+            delay(10)
             return Ok(1)
         }
 
         suspend fun provideY(): Result<Int, BindingError.BindingErrorA> {
-            delay(2)
+            delay(20)
             return Err(BindingError.BindingErrorA)
         }
 
@@ -69,6 +70,53 @@ class AsyncSuspendableBindingTest {
             }
 
             assertTrue(result is Err)
+            assertEquals(
+                expected = BindingError.BindingErrorB,
+                actual = result.error
+            )
+        }
+    }
+
+    @Test
+    fun returnsStateChangedForOnlyTheFirstAsyncBindFailWhenEagerlyCancellingBinding(): dynamic {
+        var xStateChange = false
+        var yStateChange = false
+        var zStateChange = false
+        suspend fun provideX(): Result<Int, BindingError> {
+            delay(10)
+            xStateChange = true
+            return Ok(1)
+        }
+
+        suspend fun provideY(): Result<Int, BindingError.BindingErrorA> {
+            delay(20)
+            yStateChange = true
+            return Err(BindingError.BindingErrorA)
+        }
+
+        suspend fun provideZ(): Result<Int, BindingError.BindingErrorB> {
+            delay(1)
+            zStateChange = true
+            return Err(BindingError.BindingErrorB)
+        }
+
+        return runBlockingTest {
+            val result = binding<Int, BindingError> {
+                val x = async { provideX().bind() }
+                val y = async { provideY().bind() }
+                val z = async { provideZ().bind() }
+
+                x.await() + y.await() + z.await()
+            }
+
+            assertTrue(result is Err)
+            assertEquals(
+                expected = BindingError.BindingErrorB,
+                actual = result.error
+            )
+            assertFalse(xStateChange)
+            assertFalse(yStateChange)
+            assertTrue(zStateChange)
         }
     }
 }

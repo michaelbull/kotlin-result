67cd2606a4a3ace0af363ada579893118337a5e6
Jordan Bergin <jordan.j.bergin@protonmail.com>
2021-06-10 22:15:40 +0100

    Upping Kotlin version to 1.5.10
    This commit also ups the coroutines version  to the recommended version (1.5.0) for Kotlin 1.5.10.
    
    One necessary change was casting a null parameter to resolve overload an ambiguity to CancellationException's constructor

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,8 +1,8 @@
 object Versions {
     const val dokka = "0.10.1"
-    const val kotlin = "1.4.32"
+    const val kotlin = "1.5.10"
     const val kotlinBenchmark = "0.2.0-dev-20"
-    const val kotlinCoroutines = "1.4.3"
+    const val kotlinCoroutines = "1.5.0"
     const val ktor = "1.5.4"
     const val logback = "1.2.3"
     const val versionsPlugin = "0.38.0"

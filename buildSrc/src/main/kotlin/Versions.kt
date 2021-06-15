d1908854417b6e7f3a9f2e205e723f767b47b629
Tristan <tristan@Melissas-MacBook-Pro.local>
2021-06-15 19:26:27 +0100

    resolve flaky jvm multi threaded coroutine tests
    By switching to using the jvm only (hopefully by 1.6 in common) coroutine test lib we can swap to using a fake dispatcher instead of defining threads which should resolve any odd timing issues.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,6 +3,7 @@ object Versions {
     const val kotlin = "1.5.10"
     const val kotlinBenchmark = "0.2.0-dev-20"
     const val kotlinCoroutines = "1.5.0"
+    const val kotlinCoroutinesTest = "1.5.0"
     const val ktor = "1.5.4"
     const val logback = "1.2.3"
     const val versionsPlugin = "0.38.0"

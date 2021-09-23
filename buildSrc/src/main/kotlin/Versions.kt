b8d4109eee92655745fd7750660ceea4def2cd50
pablisco <260697+pablisco@users.noreply.github.com>
2021-09-23 20:56:53 +0100

    Update Kotlin to v1.5.31

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val dokka = "0.10.1"
-    const val kotlin = "1.5.10"
+    const val kotlin = "1.5.31"
     const val kotlinBenchmark = "0.3.1"
     const val kotlinCoroutines = "1.5.0"
     const val kotlinCoroutinesTest = "1.5.0"

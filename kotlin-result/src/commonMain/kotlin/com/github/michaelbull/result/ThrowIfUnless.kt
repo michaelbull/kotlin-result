b26cd5fcdd11343213b2aa01b9dcfa72dea768e5
Joseph Cooper <cooper.joseph@gmail.com>
2021-10-26 15:33:08 +0100

    Added contracts to throwIf and throwUnless

--- kotlin-result/src/commonMain/kotlin/com/github/michaelbull/result/ThrowIfUnless.kt
+++ kotlin-result/src/commonMain/kotlin/com/github/michaelbull/result/ThrowIfUnless.kt
@@ -1,5 +1,8 @@
 package com.github.michaelbull.result
 
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
 /**
  * If the [Result] is [Err] and contains a [Throwable], throws the [error][Err.error]
  * if the [predicate] returns true.
@@ -8,8 +11,10 @@ package com.github.michaelbull.result
  */
 public inline fun <V : Any?, E : Throwable> Result<V, E>.throwIf(
     predicate: (E) -> Boolean
-): Result<V, E> =
-    onFailure { e -> if (predicate(e)) throw e }
+): Result<V, E> {
+    contract { callsInPlace(predicate, InvocationKind.AT_MOST_ONCE)  }
+    return onFailure { e -> if (predicate(e)) throw e }
+}
 
 /**
  * If the [Result] is [Err] and contains a [Throwable], throws the [error][Err.error]

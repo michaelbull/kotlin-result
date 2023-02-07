ce6131be3de9003b30105cee11b0818bd5205f55
Michael Bull <mikebull94@gmail.com>
2023-02-07 15:51:36 +0000

    Update dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,9 +1,9 @@
 object Versions {
-    const val dokka = "1.7.10"
-    const val kotlin = "1.7.10"
-    const val kotlinBenchmark = "0.4.4"
+    const val dokka = "1.7.20"
+    const val kotlin = "1.8.10"
+    const val kotlinBenchmark = "0.4.7"
     const val kotlinCoroutines = "1.6.4"
-    const val ktor = "2.0.0"
-    const val logback = "1.2.3"
-    const val versionsPlugin = "0.42.0"
+    const val ktor = "2.2.3"
+    const val logback = "1.4.5"
+    const val versionsPlugin = "0.45.0"
 }

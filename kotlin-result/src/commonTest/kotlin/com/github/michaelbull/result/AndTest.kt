d4414b1a086cb219795d649fddf79bbd4c9a4c63
Dmitry Bock <dnbokk@gmail.com>
2024-01-09 21:31:30 +0200

    Add andThen{Recover,RecoverIf,RecoverUnless}

--- kotlin-result/src/commonTest/kotlin/com/github/michaelbull/result/AndTest.kt
+++ kotlin-result/src/commonTest/kotlin/com/github/michaelbull/result/AndTest.kt
@@ -42,4 +42,122 @@ class AndTest {
             )
         }
     }
+
+    class AndThenRecover {
+        @Test
+        fun returnsValueIfOk() {
+            assertEquals(
+                expected = 5,
+                actual = Ok(5).andThenRecover { Ok(7) }.get()
+            )
+        }
+
+        @Test
+        fun returnsTransformValueIfErr() {
+            assertEquals(
+                expected = 20,
+                actual = Err(AndError).andThenRecover { Ok(20) }.get()
+            )
+        }
+    }
+
+    class AndThenRecoverIf {
+        @Test
+        fun returnsValueIfOk() {
+            fun predicate(int: Int): Boolean {
+                return int == 4000
+            }
+
+            assertEquals(
+                expected = Ok(3000),
+                actual = Ok(3000).andThenRecoverIf(::predicate) { Ok(2000) }
+            )
+        }
+
+        @Test
+        fun returnsTransformedErrorAsOkIfErrAndPredicateMatch() {
+            fun predicate(int: Int): Boolean {
+                return int == 4000
+            }
+
+            assertEquals(
+                expected = Ok(2000),
+                actual = Err(4000).andThenRecoverIf(::predicate) { Ok(2000) }
+            )
+        }
+
+        @Test
+        fun returnsTransformedErrorAsErrorIfErrAndPredicateMatch() {
+            fun predicate(int: Int): Boolean {
+                return int == 4000
+            }
+
+            assertEquals(
+                expected = Err(2000),
+                actual = Err(4000).andThenRecoverIf(::predicate) { Err(2000) }
+            )
+        }
+
+        @Test
+        fun doesNotReturnTransformationResultIfErrAndPredicateDoesNotMatch() {
+            fun predicate(int: Int): Boolean {
+                return int == 3000
+            }
+
+            assertEquals(
+                expected = Err(4000),
+                actual = Err(4000).andThenRecoverIf(::predicate) { Ok(2000) }
+            )
+        }
+    }
+
+    class AndThenRecoverUnless {
+        @Test
+        fun returnsValueIfOk() {
+            fun predicate(int: Int): Boolean {
+                return int == 4000
+            }
+
+            assertEquals(
+                expected = Ok(3000),
+                actual = Ok(3000).andThenRecoverUnless(::predicate) { Ok(2000) }
+            )
+        }
+
+        @Test
+        fun returnsTransformedErrorAsOkIfErrAndPredicateDoesNotMatch() {
+            fun predicate(int: Int): Boolean {
+                return int == 3000
+            }
+
+            assertEquals(
+                expected = Ok(2000),
+                actual = Err(4000).andThenRecoverUnless(::predicate) { Ok(2000) }
+            )
+        }
+
+        @Test
+        fun returnsTransformedErrorAsErrorIfErrAndPredicateDoesNotMatch() {
+            fun predicate(int: Int): Boolean {
+                return int == 3000
+            }
+
+            assertEquals(
+                expected = Err(2000),
+                actual = Err(4000).andThenRecoverUnless(::predicate) { Err(2000) }
+            )
+        }
+
+        @Test
+        fun doesNotReturnTransformationResultIfErrAndPredicateMatches() {
+            fun predicate(int: Int): Boolean {
+                return int == 4000
+            }
+
+            assertEquals(
+                expected = Err(4000),
+                actual = Err(4000).andThenRecoverUnless(::predicate) { Ok(2000) }
+            )
+        }
+    }
 }

73a71154e998be7daf78fedbc1b60238283e5d52
Michael Bull <mikebull94@gmail.com>
2024-03-02 16:39:14 +0000

    Add Rust reference comments for on{Success,Failure}

--- kotlin-result/src/commonMain/kotlin/com/github/michaelbull/result/On.kt
+++ kotlin-result/src/commonMain/kotlin/com/github/michaelbull/result/On.kt
@@ -5,6 +5,8 @@ import kotlin.contracts.contract
 
 /**
  * Invokes an [action] if this [Result] is [Ok].
+ *
+ * - Rust: [Result.inspect](https://doc.rust-lang.org/std/result/enum.Result.html#method.inspect)
  */
 public inline infix fun <V, E> Result<V, E>.onSuccess(action: (V) -> Unit): Result<V, E> {
     contract {
@@ -20,6 +22,8 @@ public inline infix fun <V, E> Result<V, E>.onSuccess(action: (V) -> Unit): Resu
 
 /**
  * Invokes an [action] if this [Result] is [Err].
+ *
+ * - Rust [Result.inspect_err](https://doc.rust-lang.org/std/result/enum.Result.html#method.inspect_err)
  */
 public inline infix fun <V, E> Result<V, E>.onFailure(action: (E) -> Unit): Result<V, E> {
     contract {

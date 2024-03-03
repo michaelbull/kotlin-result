c3d28ed7713a88661e50057752e7d731c9959f14
Michael Bull <mikebull94@gmail.com>
2024-03-03 13:57:22 +0000

    Simplify configuration of KotlinNpmInstallTask

--- buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+import org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask
+
 plugins {
     kotlin("multiplatform")
 }
@@ -63,4 +66,14 @@ tasks.withType<Jar> {
     from(rootDir.resolve("LICENSE")) {
         into("META-INF")
     }
+}
+
+/* https://youtrack.jetbrains.com/issue/KT-63014/Running-tests-with-wasmJs-in-1.9.20-requires-Chrome-Canary#focus=Comments-27-8321383.0-0 */
+rootProject.the<NodeJsRootExtension>().apply {
+    nodeVersion = "21.0.0-v8-canary202309143a48826a08"
+    nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
+}
+
+rootProject.tasks.withType<KotlinNpmInstallTask> {
+    args.add("--ignore-engines")
 }
--- kotlin-result/build.gradle.kts
+++ kotlin-result/build.gradle.kts
@@ -1,6 +1,4 @@
 import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
-import org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask
 
 plugins {
     `maven-publish`
@@ -24,14 +22,4 @@ kotlin {
         binaries.executable()
         nodejs()
     }
-}
-
-/* https://youtrack.jetbrains.com/issue/KT-63014/Running-tests-with-wasmJs-in-1.9.20-requires-Chrome-Canary#focus=Comments-27-8321383.0-0 */
-rootProject.the<NodeJsRootExtension>().apply {
-    nodeVersion = "21.0.0-v8-canary202309143a48826a08"
-    nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
-}
-
-rootProject.tasks.withType<KotlinNpmInstallTask>().configureEach {
-    args.add("--ignore-engines")
 }

ab06a2a3fcc35dfd0ccb39d1d183e13653df7ca4
Michael Bull <mikebull94@gmail.com>
2024-03-10 23:20:34 +0000

    Run coroutine tests on all platforms
    Some were previously restricted to specific platforms, however
    kotlinx-coroutines-test now targets them all.

--- kotlin-result-coroutines/src/commonTest/kotlin/com.github.michaelbull.result.coroutines/binding/AsyncSuspendableBindingTest.kt
+++ kotlin-result-coroutines/src/commonTest/kotlin/com.github.michaelbull.result.coroutines/binding/AsyncSuspendableBindingTest.kt
@@ -6,6 +6,8 @@ import com.github.michaelbull.result.Result
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -32,7 +34,7 @@ class AsyncSuspendableBindingTest {
             return Ok(2)
         }
 
-        val result = binding {
+        val result: Result<Int, BindingError> = binding {
             val x = async { provideX().bind() }
             val y = async { provideY().bind() }
             x.await() + y.await()
@@ -40,28 +42,28 @@ class AsyncSuspendableBindingTest {
 
         assertEquals(
             expected = Ok(3),
-            actual = result,
+            actual = result
         )
     }
 
     @Test
     fun returnsFirstErrIfBindingFailed() = runTest {
         suspend fun provideX(): Result<Int, BindingError> {
-            delay(10)
+            delay(3)
             return Ok(1)
         }
 
         suspend fun provideY(): Result<Int, BindingError.BindingErrorA> {
-            delay(20)
+            delay(3)
             return Err(BindingError.BindingErrorA)
         }
 
         suspend fun provideZ(): Result<Int, BindingError.BindingErrorB> {
-            delay(1)
+            delay(2)
             return Err(BindingError.BindingErrorB)
         }
 
-        val result = binding {
+        val result: Result<Int, BindingError> = binding {
             val x = async { provideX().bind() }
             val y = async { provideY().bind() }
             val z = async { provideZ().bind() }
@@ -78,41 +80,90 @@ class AsyncSuspendableBindingTest {
     fun returnsStateChangedForOnlyTheFirstAsyncBindFailWhenEagerlyCancellingBinding() = runTest {
         var xStateChange = false
         var yStateChange = false
+
+        suspend fun provideX(): Result<Int, BindingError> {
+            delay(2)
+            xStateChange = true
+            return Err(BindingError.BindingErrorA)
+        }
+
+        suspend fun provideY(): Result<Int, BindingError.BindingErrorB> {
+            delay(3)
+            yStateChange = true
+            return Err(BindingError.BindingErrorB)
+        }
+
+        val dispatcherA = StandardTestDispatcher(testScheduler)
+        val dispatcherB = StandardTestDispatcher(testScheduler)
+
+        val result: Result<Int, BindingError> = binding {
+            val x = async(dispatcherA) { provideX().bind() }
+            val y = async(dispatcherB) { provideY().bind() }
+
+            testScheduler.advanceTimeBy(2)
+            testScheduler.runCurrent()
+
+            x.await() + y.await()
+        }
+
+        assertEquals(
+            expected = Err(BindingError.BindingErrorA),
+            actual = result
+        )
+
+        assertTrue(xStateChange)
+        assertFalse(yStateChange)
+    }
+
+    @Test
+    fun returnsStateChangedForOnlyTheFirstLaunchBindFailWhenEagerlyCancellingBinding() = runTest {
+        var xStateChange = false
+        var yStateChange = false
         var zStateChange = false
 
         suspend fun provideX(): Result<Int, BindingError> {
-            delay(20)
+            delay(1)
             xStateChange = true
             return Ok(1)
         }
 
         suspend fun provideY(): Result<Int, BindingError.BindingErrorA> {
-            delay(10)
+            delay(20)
             yStateChange = true
             return Err(BindingError.BindingErrorA)
         }
 
         suspend fun provideZ(): Result<Int, BindingError.BindingErrorB> {
-            delay(1)
+            delay(100)
             zStateChange = true
             return Err(BindingError.BindingErrorB)
         }
 
-        val result = binding {
-            val x = async { provideX().bind() }
-            val y = async { provideY().bind() }
-            val z = async { provideZ().bind() }
+        val dispatcherA = StandardTestDispatcher(testScheduler)
+        val dispatcherB = StandardTestDispatcher(testScheduler)
+        val dispatcherC = StandardTestDispatcher(testScheduler)
 
-            x.await() + y.await() + z.await()
+        val result: Result<Unit, BindingError> = binding {
+            launch(dispatcherA) { provideX().bind() }
+
+            testScheduler.advanceTimeBy(20)
+            testScheduler.runCurrent()
+
+            launch(dispatcherB) { provideY().bind() }
+
+            testScheduler.advanceTimeBy(20)
+            testScheduler.runCurrent()
+
+            launch(dispatcherC) { provideZ().bind() }
         }
 
         assertEquals(
-            expected = Err(BindingError.BindingErrorB),
-            actual = result,
+            expected = Err(BindingError.BindingErrorA),
+            actual = result
         )
 
-        assertFalse(xStateChange)
-        assertFalse(yStateChange)
-        assertTrue(zStateChange)
+        assertTrue(xStateChange)
+        assertTrue(yStateChange)
+        assertFalse(zStateChange)
     }
 }

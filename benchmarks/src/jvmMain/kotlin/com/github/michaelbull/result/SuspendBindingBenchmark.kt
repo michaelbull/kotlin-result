09818a7d50cf13686a2f56349ac981521016c959
Michael Bull <mikebull94@gmail.com>
2024-03-10 23:21:24 +0000

    Remove redundant call to coroutineScope in SuspendBindingBenchmark

--- benchmarks/src/jvmMain/kotlin/com/github/michaelbull/result/SuspendBindingBenchmark.kt
+++ benchmarks/src/jvmMain/kotlin/com/github/michaelbull/result/SuspendBindingBenchmark.kt
@@ -9,7 +9,6 @@ import kotlinx.benchmark.OutputTimeUnit
 import kotlinx.benchmark.Scope
 import kotlinx.benchmark.State
 import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import com.github.michaelbull.result.coroutines.binding.binding as coroutineBinding
@@ -54,12 +53,10 @@ class SuspendBindingBenchmark {
         x + y
     }
 
-    private suspend fun withAsyncSuspend(): Result<Int, Error> = coroutineScope {
-        coroutineBinding {
-            val x = async { provideX().bind() }
-            val y = async { provideY().bind() }
-            x.await() + y.await()
-        }
+    private suspend fun withAsyncSuspend(): Result<Int, Error> = coroutineBinding {
+        val x = async { provideX().bind() }
+        val y = async { provideY().bind() }
+        x.await() + y.await()
     }
 
     private fun provideXBlocking(): Result<Int, Error> {

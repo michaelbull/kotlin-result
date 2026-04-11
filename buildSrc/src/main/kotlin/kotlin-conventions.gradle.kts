448cc4467964492ce195afa5411eb7d7b197dad1
Michael Bull <mikebull94@gmail.com>
2026-04-11 05:15:39 +0100

    Remove deprecated multiplatform targets
    See: https://kotlinlang.org/docs/native-target-support.html

--- buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
@@ -37,14 +37,10 @@ kotlin {
 
     linuxX64()
     linuxArm64()
-    macosX64()
-    iosX64()
     watchosSimulatorArm64()
-    watchosX64()
     watchosArm32()
     watchosArm64()
     tvosSimulatorArm64()
-    tvosX64()
     tvosArm64()
 
     /* https://kotlinlang.org/docs/native-target-support.html#tier-3 */
@@ -55,6 +51,7 @@ kotlin {
     androidNativeX64()
     mingwX64()
     watchosDeviceArm64()
+    iosX64()
 
     sourceSets {
         commonTest {
